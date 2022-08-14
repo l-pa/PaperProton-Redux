1c87aa3b6fb6c2514efc028e58240c5631d233d1
Lukas <papikdev@gmail.com>
2022-08-14 22:04:35 +0200

    about:config

--- user.js
+++ user.js
@@ -8,3 +8,12 @@ user_pref("browser.tabs.tabClipWidth", 83); 							// default is 140
 
 /* Replicate Chrome's "Not Secure" text on HTTP */
 user_pref("security.insecure_connection_text.enabled", true);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 150)
+user_pref("mousewheel.min_line_scroll_amount", 25)
+user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100)
+user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 50)
+user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_max_size", 100)
+user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5)
+user_pref("gfx.webrender.all", true)
+user_pref("mousewheel.acceleration.factor", 3)
+user_pref("mousewheel.acceleration.start", 7)
